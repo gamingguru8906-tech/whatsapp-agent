f27fb206b6eacb253bac89af7b771cd437d7a388
shashank agrawwal <shashankmmj@gmail.com>
2026-09-23 17:03:59 +0530

    fix: explicitly used Helvetica-Bold for VESHANNASTRO text in invoice PDF

--- invoice-generator.js
+++ invoice-generator.js
@@ -36,7 +36,7 @@ function generateInvoice(data) {
       doc.fontSize(12).fillColor(secondaryColor).text(`#${data.invoiceNumber}`, 50, 140);
       
       // "Logo" representation in top right
-      doc.fillColor(primaryColor).fontSize(16).text('VESHANNASTRO', 380, 115, { bold: true, align: 'right' });
+      doc.fillColor(primaryColor).fontSize(16).font('Helvetica-Bold').text('VESHANNASTRO', 380, 115, { align: 'right' }).font('Helvetica');
 
       // Balance Due (Paid)
       doc.fontSize(10).fillColor(primaryColor).text('Balance Due', 50, 180);
